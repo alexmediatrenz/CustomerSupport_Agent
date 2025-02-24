36fe38f447b2ba07bf4b5b111b1e997b894642c8
alexmediatrenz <alexsmith.mediatrenz@gmail.com>
2025-02-24 21:15:45 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ChatWindow from './ChatWindow';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ChatWindow />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
